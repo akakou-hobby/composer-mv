7c5868994de8c0d425875c726814c2d8aada6c22
akakou <akakou571@gmail.com>
2023-01-03 02:19:02 +0900

    [add] sorting utilities

--- scripts/utils.js
+++ scripts/utils.js
@@ -7,3 +7,50 @@ function sleep(msec) {
         setTimeout(function () { resolve() }, msec);
     })
 }
+
+
+function compareString(a, b) {
+    if (a < b)
+        return -1
+
+    if (a > b)
+        return 1
+
+    return 0
+}
+
+
+function reverseString(str) {
+    var splitString = str.split("")
+    var reverseArray = splitString.reverse()
+    var joinArray = reverseArray.join("")
+
+    return joinArray
+}
+
+function compareNotes(a, b) {
+    const reversedA = reverseString(a)
+    const reversedB = reverseString(b)
+
+    const lastA = reversedA[0]
+    const lastB = reversedB[0]
+
+    console.log(lastA, lastB)
+
+    if (lastA != lastB) {
+        return lastA - lastB
+    }
+
+    return compareString(
+        reverseString(a),
+        reverseString(b)
+    )
+}
+
+function sortNotes(notes) {
+    notes.sort(compareNotes);
+
+    return notes
+}
+
+
